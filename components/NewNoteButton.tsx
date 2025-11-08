37584b557a256c7e796d67b290927b09f082b34a
rudalson <rudalson@gmail.com>
2025-11-08 19:16:15 +0900

    added: new note and update the content text

--- components/NewNoteButton.tsx
+++ components/NewNoteButton.tsx
@@ -2,18 +2,50 @@
 
 import { User } from "@supabase/supabase-js";
 import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
+import { toast } from "sonner"
+import { createNoteAction } from "@/actions/notes";
 
 type Props = {
   user: User | null;
 };
 
 function NewNoteButton({ user }: Props) {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleClickNewNoteButton = async () => {
+    if (!user) {
+      router.push("/login");
+    } else {
+      setLoading(true);
+
+      const uuid = uuidv4();
+      await createNoteAction(uuid);
+      router.push(`/?noteId=${uuid}&toastType=newNote`);
+
+      toast("Success" ,{
+          description: "successfully created note",
+        //   variant: "destructive",
+        });
+
+
+      setLoading(false);
+    }
+  };
+
   return (
     <Button
+      onClick={handleClickNewNoteButton}
       variant="secondary"
       className="w-24"
+      disabled={loading}
     >
-        NewNoteButton
+      {loading ? <Loader2 className="animate-spin" /> : "New Note"}
     </Button>
   );
 }
